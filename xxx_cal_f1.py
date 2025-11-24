1c3200ac1c87e282e019637ba68ba0b4c5b92ba9
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-11-24 11:57:02 +0900

    add interaction

--- xxx_cal_f1.py
+++ xxx_cal_f1.py
@@ -3,7 +3,7 @@ import os, glob
 
 
 # ==== 読み込むログファイルをここで並べる ====
-dir_name = "robertaIr2e-5_elseIr1e-4_hiddenDim768_speakerStateDim384_timeDim0_head6_localWindowNum0_dropout0.1_AddEmotionGRU"
+dir_name = "robertaIr2e-5_elseIr1e-4_hiddenDim768_speakerStateDim384_timeDim0_head6_localWindowNum0_dropout0.1_AddInteraction"
 
 target = os.path.join("logs", "test", "IEMOCAP", dir_name)
 if (not os.path.isdir(target)):
